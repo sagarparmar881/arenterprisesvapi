b45880beff4449780233239c9532ba16de57ef87
Sagar Parmar <47896458+sagarparmar881@users.noreply.github.com>
2023-02-19 22:54:41 +0530

    Added CallNow.vue (Block for Contact, Chat, Email)

---
+++ components/CallNow.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="subscribe-area-3 pb-100 p-3 mb-30">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-lg-4 mt-30">
+          <div class="subscribe-style-3 text-center">
+            <h2>Contact Us</h2>
+            <div class="discover-more-btn btn-hover text-center">
+              <a href="tel:9824139769" style="font-weight: 900;">
+                <i class="pe-7s-call" style="font-weight: bold;"></i> +91 98241 39769 </a>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-lg-4 mt-30">
+          <div class="subscribe-style-3 text-center">
+            <h2>Chat With Us</h2>
+            <div class="discover-more-btn btn-hover text-center">
+              <a href="https://wa.me/919824139769/?text=Hello!" style="font-weight: 900;">
+                <i class="pe-7s-chat" style="font-weight: bold;"></i> +91 98241 39769 </a>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-lg-4 mt-30">
+          <div class="subscribe-style-3 text-center">
+            <h2>Email Us</h2>
+            <div class="discover-more-btn btn-hover text-center">
+              <a href="mailto:sales@arenterprisesvapi.com" style="font-weight: 900;">
+                <i class="pe-7s-mail" style="font-weight: bold;"></i>sales@arenterprisesvapi.com</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  export default {};
+</script>
