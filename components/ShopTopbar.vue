347cd5fad5cd589690ec5cce49c844dcb2ad8524
Sagar Parmar <47896458+sagarparmar881@users.noreply.github.com>
2023-02-19 18:08:05 +0530

    New Category Bar Vue

---
+++ components/ShopTopbar.vue
@@ -0,0 +1,89 @@
+<template>
+    <div class="sidebar-style" :class="classes">
+        <!-- <div class="sidebar-widget">
+            <h4 class="pro-sidebar-title">Search</h4>
+            <div class="pro-sidebar-search mb-50 mt-25">
+                <form class="pro-sidebar-search-form">
+                    <input type="text" placeholder="Search here...">
+                    <button>
+                        <i class="pe-7s-search"></i>
+                    </button>
+                </form>
+            </div>
+        </div> -->
+
+        <!-- color widget  -->
+        <!-- <div class="sidebar-widget mt-50">
+            <h4 class="pro-sidebar-title">Colour</h4>
+            <ul class="sidebar-widget-list mt-20">
+                <li class="sidebar-widget-list-left" v-for="(color, index) in colorList" :key="index" >
+                    <n-link :to="`?color=${slugify(color)}`">
+                        <span class="check-mark"></span>
+                        {{ color }}
+                    </n-link>
+                </li>
+            </ul>
+        </div> -->
+
+        <!-- size widget  -->
+       <!--  <div class="sidebar-widget mt-50">
+            <h4 class="pro-sidebar-title">Sizes</h4>
+            <ul class="sidebar-widget-list mt-20">
+                <li class="sidebar-widget-list-left" v-for="(size, index) in sizeList" :key="index" >
+                    <n-link :to="`?size=${slugify(size)}`">
+                        <span class="check-mark"></span>
+                        {{ size }}
+                    </n-link>
+                </li>
+            </ul>
+        </div> -->
+
+        <!-- New Categories widget  -->
+        <div class="sidebar-widget sidebar-widget__tag mt-60">
+            <!-- <h4 class="pro-sidebar-title">Categories</h4> -->
+            <div class="sidebar-widget-tag mt-30">
+                <ul class="list-group flex-column flex-sm-row">
+                    <li class="list-group-item borderless" v-for="(category, index) in categoryList" :key="index">
+                    <n-link :to="`?category=${slugify(category)}`">
+                        <span class="check-mark"></span>
+                        {{ category }}
+                    </n-link>
+                </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ["classes"],
+        computed: {
+            categoryList() {
+                return this.$store.getters.categoryList
+            },
+            sizeList() {
+                return this.$store.getters.sizeList
+            },
+            colorList() {
+                return this.$store.getters.colorList
+            },
+            tagList() {
+                return this.$store.getters.tagList
+            }
+        },
+
+        methods: {
+            slugify(text) {
+                return text
+                    .toString()
+                    .toLowerCase()
+                    .replace(/\s+/g, "-") // Replace spaces with -
+                    .replace(/[^\w-]+/g, "") // Remove all non-word chars
+                    .replace(/--+/g, "-") // Replace multiple - with single -
+                    .replace(/^-+/, "") // Trim - from start of text
+                    .replace(/-+$/, ""); // Trim - from end of text
+            }
+        }
+    };
+</script>
