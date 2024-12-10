0c3afa21e7c28cd0a7455e2ab7bc66ed38d325a0
Sagar Parmar <47896458+sagarparmar881@users.noreply.github.com>
2024-12-10 22:40:32 +0530

    Update AboutMessage.vue

--- components/AboutMessage.vue
+++ components/AboutMessage.vue
@@ -15,7 +15,7 @@
         data() {
             return {
                 title: "Welcome To A R Enterprises",
-                subTitle: "Who Are We",
+                subTitle: "Who Are We?",
                 desc: "Welcome to A R Enterprises, a go-to industrial for all products, like detergent powder, multipurpose liquids, and other cleaning materials. We are committed to providing you with the best products possible, with a focus on three qualities: dependability, customer service, and distinctiveness.<br><br>A R Enterprises, established in January 2005 by Ashraf Agwan and Reshma Agwan, has come a long way from employing maids in Vapi. When Ashraf Agwan first got started, his love for creating cleaning equipment inspired him to do everything himself and provided him the motivation to develop his labour of love and inspiration into a successful online company. <br><br>Our goal is to give our customers the finest products possible. We truly hope you find pleasure in our offerings equally as we do. Please get in touch with us if there are any queries or comments.<br><br>Yours sincerely,<br>Ashraf Agwan,<br>Co-founder (AR Enterprises)."
             }
         },
